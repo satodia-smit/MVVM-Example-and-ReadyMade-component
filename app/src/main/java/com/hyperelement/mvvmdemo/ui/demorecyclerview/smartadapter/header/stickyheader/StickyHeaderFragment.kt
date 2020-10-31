7ef0b232832662c31e15b18f71e85a9f95194045
smit_satodia <smit.satodia@rampgroup.com>
2020-10-31 13:35:56 +0530

    Updated StickyHeader dataset, now it will load the data from dummy json, which is mimic real time behaviour

--- app/src/main/java/com/hyperelement/mvvmdemo/ui/demorecyclerview/smartadapter/header/stickyheader/StickyHeaderFragment.kt
+++ app/src/main/java/com/hyperelement/mvvmdemo/ui/demorecyclerview/smartadapter/header/stickyheader/StickyHeaderFragment.kt
@@ -2,12 +2,12 @@ package com.hyperelement.mvvmdemo.ui.demorecyclerview.smartadapter.header.sticky
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
 import com.hyperelement.mvvmdemo.R
 import com.hyperelement.mvvmdemo.arch.BaseFragment
 import com.hyperelement.mvvmdemo.data.datasources.models.generic.Continent
 import com.hyperelement.mvvmdemo.data.datasources.models.generic.Country
 import com.hyperelement.mvvmdemo.databinding.FragmentStickyHeaderBinding
-import com.hyperelement.mvvmdemo.utilities.ExtraUtils
 import kotlinx.android.synthetic.main.fragment_sticky_header.*
 import smartadapter.SmartRecyclerAdapter
 import smartadapter.stickyheader.StickyHeaderItemDecorationExtension
@@ -25,9 +25,6 @@ class StickyHeaderFragment :
         super.onViewCreated(view, savedInstanceState)
         getSpecificBinding<FragmentStickyHeaderBinding>()?.viewModel = viewModel
 
-        val mJsonContinentCountry = ExtraUtils.getJsonFromAssets(requireContext(), "Continent.json")
-        Timber.d("JSON STRING \n $mJsonContinentCountry")
-
         adapter = SmartRecyclerAdapter.empty()
             .map(Continent::class, StickyHeaderParentVH::class)
             .map(Country::class, StickyHeaderChildVH::class)
@@ -37,29 +34,22 @@ class StickyHeaderFragment :
                 )
             )
             .into(rvContinent)
+        viewModel.loadContinentCountry()
 
-        val mContinentArray = arrayListOf<Continent>()
-        mContinentArray.add(
-            Continent(
-                "Africa",
-                arrayListOf(Country("Algeria - Algiers"), Country("Angola - Luanda"))
-            )
-        )
-        mContinentArray.add(
-            Continent(
-                "Africa1",
-                arrayListOf(Country("Algeria - Algiers1"), Country("Angola - Luanda1"))
-            )
-        )
+        viewModel.mContinentCountryList.observe(viewLifecycleOwner, Observer
+        {
 
-        val mArray2 = arrayListOf<Any>()
-        for (continent in mContinentArray) {
-            mArray2.add(Continent(continent.mContinentName))
-            for (country in continent.mCountries!!) {
-                mArray2.add(Country(country.mCountryName))
+            val mArray = arrayListOf<Any>()
+            var mOldContinent = ""
+            for (continent in it.sortedWith(compareBy({ it.continent }))) {
+                Timber.d("ITEM $continent")
+                if (mOldContinent != continent.continent) {
+                    mArray.add(Continent(continent.continent))
+                    mOldContinent = continent.continent
+                }
+                mArray.add(Country(continent.name))
             }
-        }
-
-        adapter.addItems(mArray2.toMutableList())
+            adapter.addItems(mArray.toMutableList())
+        })
     }
 }
--- app/src/main/java/com/hyperelement/mvvmdemo/ui/demorecyclerview/smartadapter/header/stickyheader/StickyHeaderParentVH.kt
+++ app/src/main/java/com/hyperelement/mvvmdemo/ui/demorecyclerview/smartadapter/header/stickyheader/StickyHeaderParentVH.kt
@@ -6,16 +6,6 @@ import com.hyperelement.mvvmdemo.R
 import com.hyperelement.mvvmdemo.data.datasources.models.generic.Continent
 import smartadapter.viewholder.SmartViewHolder
 
-//class StickyHeaderParentVH(parentView: ViewGroup) :
-//    SmartViewHolder<Continent>(
-//    parentView.inflate<ItemStickyHeaderParentBinding>(R.layout.item_sticky_header_parent).root
-//) {
-//    override fun bind(item: Continent) {
-//        val binding = DataBindingUtil.getBinding<ItemStickyHeaderParentBinding>(itemView)
-//        binding?.dataItem = item
-//    }
-//}
-
 class StickyHeaderParentVH(parentView: ViewGroup) :
     SmartViewHolder<Continent>(parentView, R.layout.item_sticky_header_parent) {
 
